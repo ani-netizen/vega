7990d2e54a828053c7b15a2116134e83227ef3e5
yash kesharwani <imyash2106@gmail.com>
2024-03-07 00:51:02 +0530

    resource css Changed

--- client/src/components/CompanyCard/CompanyCard.jsx
+++ client/src/components/CompanyCard/CompanyCard.jsx
@@ -4,21 +4,19 @@ import { Link } from "react-router-dom";
 function CompanyCard({ company }) {
   return (
     <Link to={`/${company._id}`}>
-      <div class="w-full max-w-sm border hover:scale-105 duration-500 rounded-lg shadow bg-gray-800 border-gray-700 p-4">
-        <div class="flex flex-col items-center">
-          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+      <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 shadow overflow-hidden">
+        <div className="flex flex-col  p-4">
+          <h5 className="mb-3 text-3xl font-bold tracking-tight text-gray-900 dark:text-white items-center flex flex-col">
             {company.name}
           </h5>
-          <p class="text-sm text-gray-400 overflow-ellipsis">
-            {company.description}
-          </p>
-          <div class="text-gray-400 mt-4 md:mt-6 text-sm">
-            <p>
-              <strong className="text-white">Job Roles:</strong>{" "}
+          <p className="mb-3 text-gray-500 dark:text-gray-300 mt-4"><strong className="p-4 text-blue-600 rounded-ss-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Description:</strong>{company.description}</p>
+          <div className="text-sm text-gray-500">
+            <p className="mb-4 mb-3 text-gray-500 dark:text-gray-300">
+              <strong className="p-4 text-blue-600 rounded-ss-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Job Roles:</strong>
               {company.jobRoles.join(", ")}
             </p>
-            <p>
-              <strong className="text-white">Eligibility Criteria:</strong>{" "}
+            <p className="mb-3 text-gray-500 dark:text-gray-300">
+              <strong className="p-4 text-blue-600 rounded-ss-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Eligibility Criteria:</strong>
               {company.eligibilityCriteria}
             </p>
           </div>
