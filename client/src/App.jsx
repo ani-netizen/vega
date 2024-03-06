a794954ab11f77c456f71d82948648ed4e428b5f
Jaideep Pramod Jambhale <jaideep.jambhale@gmail.com>
2024-03-06 21:11:40 +0530

    created contactus and homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import CompanyProfiles from "./pages/CompanyProfiles";
 import Resources from "./pages/Resources";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import ContactUs from "./pages/Contactus";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<CompanyProfiles />} />
+        <Route exact path="/" element={<Home />} />
         {/* <Route path="/ipc" element={Calendar} /> */}
         <Route path="/cip" element={<CompanyProfiles />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+
+
         <Route path="/pma" element={<Resources />} />
       </Routes>
     </div>
